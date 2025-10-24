f180606b4b9ca55a3edbe5c26989c04e0fdca77c
Grover Smith <crimson.manticore@gmail.com>
2025-10-24 10:59:54 -0400

    Update MfmEnhancementRepository.java

--- src/main/java/com/warplay/repository/MfmEnhancementRepository.java
+++ src/main/java/com/warplay/repository/MfmEnhancementRepository.java
@@ -2,6 +2,7 @@ package com.warplay.repository;
 
 import com.warplay.entity.MfmDetachment;
 import com.warplay.entity.MfmEnhancement;
+import com.warplay.entity.MfmFaction;
 import com.warplay.entity.MfmVersion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
